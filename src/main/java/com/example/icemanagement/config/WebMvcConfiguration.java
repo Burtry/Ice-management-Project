a688dcb7368f77d8925acbb30762622fd28de0d6
Burtry <1971797331@qq.com>
2023-11-25 18:56:52 +0800

    update code

--- src/main/java/com/example/icemanagement/config/WebMvcConfiguration.java
+++ src/main/java/com/example/icemanagement/config/WebMvcConfiguration.java
@@ -9,4 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 @Slf4j
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    /**
+     * 静态映射
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }

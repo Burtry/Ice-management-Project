e9eba99cf9f8263564521f6893019e84c56fd5d4
Burtry <1971797331@qq.com>
2023-12-09 16:09:57 +0800

    update code---场地分页查询

--- src/main/java/com/example/icemanagement/controller/admin/SpaceController.java
+++ src/main/java/com/example/icemanagement/controller/admin/SpaceController.java
@@ -1,17 +1,17 @@
 package com.example.icemanagement.controller.admin;
 
+import com.example.icemanagement.common.result.PageResult;
 import com.example.icemanagement.common.result.Result;
+import com.example.icemanagement.pojo.dto.EquipmentPageQueryDTO;
 import com.example.icemanagement.pojo.dto.SpaceDTO;
+import com.example.icemanagement.pojo.dto.SpacePageQueryDTO;
 import com.example.icemanagement.pojo.entity.Space;
 import com.example.icemanagement.service.SpaceService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 场地管理
@@ -36,6 +36,15 @@ public class SpaceController {
         log.info("新增场地:{}", spaceDTO);
         spaceService.insert(spaceDTO);
         return Result.success();
+
+    }
+
+    @Operation(summary = "场地查询")
+    @GetMapping("/page")
+    public Result<PageResult> page(SpacePageQueryDTO spacePageQueryDTO) {
+        log.info("场地查询:{}", spacePageQueryDTO);
+        PageResult pageResult = spaceService.pageQuery(spacePageQueryDTO);
+        return Result.success(pageResult);
     }
 
 
--- src/main/java/com/example/icemanagement/service/ServiceImpl/SpaceServiceImpl.java
+++ src/main/java/com/example/icemanagement/service/ServiceImpl/SpaceServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.icemanagement.service.ServiceImpl;
 
+import com.example.icemanagement.common.result.PageResult;
 import com.example.icemanagement.mapper.SpaceMapper;
 import com.example.icemanagement.pojo.dto.SpaceDTO;
+import com.example.icemanagement.pojo.dto.SpacePageQueryDTO;
 import com.example.icemanagement.pojo.entity.Space;
 import com.example.icemanagement.service.SpaceService;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +32,17 @@ public class SpaceServiceImpl implements SpaceService {
         spaceMapper.insert(space);
 
 
+    }
+
+    /**
+     * 场地分页查询
+     * @param spacePageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(SpacePageQueryDTO spacePageQueryDTO) {
+        PageHelper.startPage(spacePageQueryDTO.getPage(),spacePageQueryDTO.getPageSize());
+        Page<Space> page = spaceMapper.pageQuery(spacePageQueryDTO);
+        return new PageResult(page.getTotal(), page.getResult());
     }
 }
